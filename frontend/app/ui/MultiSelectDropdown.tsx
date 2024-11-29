6f9d54f7362542691b54de0bea50ce699e9843ed
Sai-Divya-Gunti <saidivyagunti@gmail.com>
2024-11-29 12:39:58 +0530

    integration changes

--- frontend/app/ui/MultiSelectDropdown.tsx
+++ frontend/app/ui/MultiSelectDropdown.tsx
@@ -26,6 +26,7 @@ export function MultiSelectDropdown(props: any) {
       target: { value },
     } = event;
     setValue(typeof value === "string" ? value.split(",") : value);
+    props?.onChange(value);
   };
 
   return (
