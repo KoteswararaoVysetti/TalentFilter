18856226397a996e7733f0c932d413c57a5ab705
Sai-Divya-Gunti <saidivyagunti@gmail.com>
2024-12-02 18:21:45 +0530

    worked on css changes

--- frontend/src/app/upload-form/page.tsx
+++ frontend/src/app/upload-form/page.tsx
@@ -11,19 +11,22 @@ import { useCallback, useState } from "react";
 import { Endpoints } from "@/src/common/constants";
 
 const Skills = [
-    { id: 1, name: ".Net" },
-    { id: 2, name: "Java" },
-    { id: 3, name: "React" },
-    { id: 3, name: "SignalR" },
-    { id: 3, name: "Angular" },
-    { id: 3, name: "TypeScript" },
-    { id: 3, name: "Azure" },
-    { id: 3, name: "AWS" },
-    { id: 3, name: "PostgreSQL" },
-    { id: 4, name: "SQL" },
-    { id: 5, name: "Python" },
-  ];
-const Roles = [{ id: 1, value: ".Net Fullstack Developer" }, { id: 2, value: "Python Fullstack Developer" }];
+  { id: 1, name: ".Net" },
+  { id: 2, name: "Java" },
+  { id: 3, name: "React" },
+  { id: 3, name: "SignalR" },
+  { id: 3, name: "Angular" },
+  { id: 3, name: "TypeScript" },
+  { id: 3, name: "Azure" },
+  { id: 3, name: "AWS" },
+  { id: 3, name: "PostgreSQL" },
+  { id: 4, name: "SQL" },
+  { id: 5, name: "Python" },
+];
+const Roles = [
+  { id: 1, value: ".Net Fullstack Developer" },
+  { id: 2, value: "Python Fullstack Developer" },
+];
 
 export default function UploadForm() {
   const router = useRouter();
@@ -33,49 +36,44 @@ export default function UploadForm() {
   const [resume, setResume] = useState<File | null>(null);
 
   const formSubmit = useCallback(async () => {
-    if(!role || !skills || !resume) {
-        console.log(role, skills, resume)
-        return;
+    if (!role || !skills || !resume) {
+      return;
     }
     async function UploadResume(role: string, skills: string, file: File) {
-        const formdata = new FormData();
-        formdata.append('role', role);
-        formdata.append('skills', skills);
-        formdata.append('file', file, file.name, );
-        console.log(formdata);
-        const requestOptions = {
-          method: "POST",
-          body: formdata,
-        };
-        try {
-          const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}${Endpoints.ProcessResume}`; 
-          console.log(formdata);
-          const response = await fetch(url, requestOptions);
-            console.log(response);
-            
-          if (!response.ok) {
-            throw new Error("An error occured");
-          }
-          const res = await response.json();
-          console.log(res);
-          return res;
-        } catch (ex) {
-          console.log("Error fetching data:", ex);
-          return null;
+      const formdata = new FormData();
+      formdata.append("role", role);
+      formdata.append("skills", skills);
+      formdata.append("file", file, file.name);
+      const requestOptions = {
+        method: "POST",
+        body: formdata,
+      };
+      try {
+        const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}${Endpoints.ProcessResume}`;
+        const response = await fetch(url, requestOptions);
+
+        if (!response.ok) {
+          throw new Error("An error occured");
         }
+        const res = await response.json();
+        return res;
+      } catch (ex) {
+        return null;
       }
-  const response = await UploadResume(role, skills, resume);
-  if (response) {
-    dispatch(updateResults(response));
-    router.push("/results");
-  }
-}, [role, skills, resume])
+    }
+    const response = await UploadResume(role, skills, resume);
+    if (response) {
+      dispatch(updateResults(response));
+      router.push("/results");
+    }
+  }, [role, skills, resume]);
 
   return (
     <div className="wrapper flex flex-col justify-center	items-center m-2.5">
       <div className="container w-2/4	h-4/5	flex flex-col justify-center items-center overflow-auto">
         <form className="flex flex-col gap-20">
           <div>
+            <FileUpload onChange={setResume} />
             <MultiSelectDropdown
               label={"Skills"}
               list={Skills}
@@ -84,9 +82,10 @@ export default function UploadForm() {
             <Dropdown
               label={"Role"}
               list={Roles}
-              onChange={val => {setRole(Roles.find(s => s.id == val)?.value || null)}}
+              onChange={(val: any) => {
+                setRole(Roles.find((s) => s.id == val)?.value || null);
+              }}
             />
-            <FileUpload onChange={setResume} />
           </div>
           <Button
             variant="contained"
